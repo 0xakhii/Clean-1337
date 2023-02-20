eba7f18f4b9785edb996290cade20c7311930bc5
cat161 <ojamal@student.1337.ma>
2023-02-20 11:12:33 +0100

    making it executable

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: ojamal <ojamal@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/20 09:51:00 by ojamal            #+#    #+#              #
-#    Updated: 2023/02/20 11:04:04 by ojamal           ###   ########.fr        #
+#    Updated: 2023/02/20 11:12:07 by ojamal           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,7 +20,7 @@ printf "• Downloading the clean script...\n"
 sleep 1;
 printf "•$GREEN Done $RESET\n"
 #making it executable
-chmod +x $HOME/.clean.sh
+chmod 777 $HOME/.clean.sh
 #defining the command : clean
 alias clean="sh $HOME/.clean.sh"
 #making the alias permanent
